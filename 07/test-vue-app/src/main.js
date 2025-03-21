a7af7bf7c520798115b6877d1d56fcefb5792260
khanian <khanylee@gmail.com>
2025-03-21 10:27:15 +0900

    modified example 07

--- 07/test-vue-app/src/main.js
+++ 07/test-vue-app/src/main.js
@@ -2,5 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import CheckboxItem from './components/CheckboxItem.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('checkbox-item', CheckboxItem)
+    .mount('#app')
