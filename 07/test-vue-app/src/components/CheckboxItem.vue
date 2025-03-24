c4efbfbba7f825fb419da42fae47da44b404e676
khanian <khanylee@gmail.com>
2025-03-24 18:02:51 +0900

    modified example 07

--- 07/test-vue-app/src/components/CheckboxItem.vue
+++ 07/test-vue-app/src/components/CheckboxItem.vue
@@ -9,7 +9,13 @@
         name: "CheckboxItem",
         props : {
             id: [Number, String],
-            name: String,
+            // name: String,
+            name: {
+                validator(v) {
+                    return typeof(v) !== 'string' ? false : 
+                    v.trim().length >= 4 ? true : false
+                },
+            },
             checked: {
                 type: Boolean,
                 required: false,
--- 07/test-vue-app/src/main.js
+++ 07/test-vue-app/src/main.js
@@ -1,9 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-// import App from './App.vue'
+import App from './App.vue'
 // import App from './App2.vue'
-import App from './App3.vue'
+// import App from './App3.vue'
 // import CheckboxItem from './components/CheckboxItem.vue'
 
 createApp(App)
