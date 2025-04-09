e13c59c911cc484ab8397b4fe2c621c196c37816
khanian <khanylee@gmail.com>
2025-04-09 16:43:28 +0900

    modified example 08

---
+++ 08/slot-test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NoSlotTest />
+  </div>
+</template>
+
+<script>
+import NoSlotTest from './components/NoSlotTest.vue';
+
+  export default {
+    name: "App",
+    components: {
+      NoSlotTest
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
