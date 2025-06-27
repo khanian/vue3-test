c8a232051fa0e64ea15083d1bf5c606912560e62
khanian <khanylee@gmail.com>
2025-06-27 11:00:04 +0900

    modified example 08

---
+++ 08/provide-inject-teleport-test/src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.config.unwrapInjectedRef = true
+window.vm = app.mount('#app')
+// createApp(App).mount('#app')
